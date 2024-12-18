a628476a2325ce11c2c06c51e8a41c3b69b15fb9
denizgrkn <denizefe191@gmail.com>
2024-12-19 00:32:35 +0300

    City class created only need names for cities sos no such methods here

---
+++ src/City.java
@@ -0,0 +1,22 @@
+public class City {
+    private String name;
+
+// in this city class i did not need any functions or parameters because my route class and CountryMap class take care of most of it so i just added a Name for city class ang get it with getName
+
+
+    public City(){
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void distanceTo(){
+
+    }
+}
